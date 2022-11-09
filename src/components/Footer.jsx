994d4e89335f2a412cec3270bbaf90138e4346bd
Zinaid Kapić <zinaid_kapic@hotmail.com>
2022-11-09 15:21:09 +0100

    Test changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => (
     <footer className='w-full bg-primary flex justify-center mt-20'>
-      <p className='text-[14px] text-[#4F4F4F] py-10 font-poppins text-normal'>© Portfolio 2022</p>
+      <p className='text-[14px] text-[#4F4F4F] py-10 font-poppins text-normal'>© Portfolio 2022 </p>
     </footer>
 )
 
