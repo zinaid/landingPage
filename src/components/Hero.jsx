547e00c8f11e2878e5e06709090abad153fe07b8
Zinaid Kapić <zinaid_kapic@hotmail.com>
2022-11-10 15:00:37 +0100

    Added gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ import Button from './Button'
 
 const Hero = () => {
   return (
-    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-        <div className={`flex md:flex-row flex-col justify-between items-center xl:px-0 sm:px-16 w-full px-6`}>
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY} min-h-[100vh]`}>
+        <div className={`flex md:flex-row flex-col justify-between items-center  xl:px-0 sm:px-16 w-full px-6`}>
             <div className='flex flex-col'>
                 <h1 className={`custom-heading font-poppins`}>
                     Hi, I am <br className='sm:block hidden' /><span className='hover:text-secondary'>Zinaid Kapić</span>.
@@ -22,11 +22,18 @@ const Hero = () => {
                 </a>
                 </div>
             </div>
-            <div class="md:mt-0 mt-10 border-2 rounded-[50%] border-[#1e1d1d33]">
-                <div className='flex flex-col custom-border'> 
-                    <img src={banner} alt="banner" className='rounded-[50%]'/>
+            <div class="md:mt-0 mt-10 border-2 rounded-[50%] border-[#1e1d1d33] ">
+                <div className='flex flex-col custom-border relative z-[0]'> 
+                    <img src={banner} alt="banner" className='rounded-[50%] z-[5] '/>
+                    {/* gradient start */}
+                    <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+                    <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+                    <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+                    {/* gradient end */}
                 </div>
+                
             </div>
+            
         </div>
     </section>
   )
